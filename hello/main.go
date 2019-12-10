9cc1f21aeacdb439b9b7868f9d6db3d3018425b6
JimTheMan <mrdotjim@gmail.com>
2019-12-10 08:58:40 -0500

    nice but no params

--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,8 +20,11 @@ type Response events.APIGatewayProxyResponse
 func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
+	fmt.Println("Printing the ctx ", ctx)
+
+	derp := fmt.Sprintf("%d", ctx.params)
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
+		"message": derp,
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
@@ -33,7 +37,7 @@ func Handler(ctx context.Context) (Response, error) {
 		Body:            buf.String(),
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
+			"X-MyCompany-Func-Reply": "world-handler",
 		},
 	}
 
